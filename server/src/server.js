8a102a3655120224c9ec02b093237358e4d1a37f
semegen <iurii.semegen@webmil.eu>
2026-05-21 14:11:26 +0300

    Add health check endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -5,3 +5,9 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+  });
+});
